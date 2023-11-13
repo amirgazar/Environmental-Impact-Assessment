728d0db5a96799d3be5031cbcdc24a2f4571cfe0
Amir Mortazavigazar <amirgazar@vt.edu>
2023-11-12 23:07:57 -0500

    Update Figure_4.py

--- misc/Figure_4.py
+++ misc/Figure_4.py
@@ -4,20 +4,20 @@ df = pd.read_csv("/content/drive/MyDrive/Colab Notebooks/hydro_var_aug23.csv")
 
 fig, ax1 = plt.subplots()
 
-# Bar plot for INSTALLED 
+# Bar plot for INSTALLED
 ax1.bar(df['Year'], df['INSTALLED'], label='Installed Generation Capacity (MW)',color='black')
 ax1.set_ylabel('Installed Generation Capacity (MW)')
 
 ax2 = ax1.twinx()
 
-# Bar plot for INTERTIE 
+# Bar plot for INTERTIE
 ax2.bar(df['Year'], df['INTERTIE'], label='Intertie Capacity (MW)', color='pink', alpha=0.8)
 ax2.set_ylabel('Intertie Capacity (MW)')
 
-# Line plot for PRICE 
+# Line plot for PRICE
 ax3 = ax1.twinx()
-ax3.plot(df['Year'], df['PRICE'], color='red', marker='o', linestyle='-', label='Price Difference ($CAD/kWh)')
-ax3.set_ylabel('Price difference between U.S. and Quebec ($CAD/kWh)')
+ax3.plot(df['Year'], df['PRICE'], color='red', marker='o', linestyle='-', label='Price Difference (\$CAD kWh$^{-1}$)')
+ax3.set_ylabel('Price difference between NE USA and Quebec (\$CAD kWh$^{-1}$)', fontsize=9)
 
 ax3.spines['right'].set_position(('outward', 60))
 ax3.yaxis.label.set_color('red')
